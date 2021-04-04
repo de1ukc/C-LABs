ffe86382e9be7f104758f19ba8984c4976a3e85f
Aleh <79203780+de1ukc@users.noreply.github.com>
2021-04-04 20:30:37 +0300

    Update lab_3.c

--- Aleh Asadchy/lab_3/lab_3.c
+++ Aleh Asadchy/lab_3/lab_3.c
@@ -7,28 +7,33 @@ int main()
     printf("%s","Enter size of array : ");
     scanf("%d", &N);
     int i = 0,j = 0;
-    int **a;
-    int **flags;
-    int *b;
+    int **a;     // source array
+    int **flags; // map
+    int *b;      // output array
     a = (int**)malloc (sizeof(int*)*N);
     flags = (int**)malloc (sizeof(int*)*N);
     b = (int*)malloc(sizeof(int*)*(N*N));
+    
     for (int i = 0 ; i < N ; ++i)
     {
         a[i] =(int*)malloc(sizeof(int*)*(N));
     }
+    
     for (int i = 0 ; i < N; ++i)
     {
         flags[i] =(int*)malloc(sizeof(int*)*(N));
     }
+    
 for(int i = 0; i < N; i++)
 for(int j = 0; j < N; j++)
     flags[i][j] = 0;
+        
 for (int i = 0; i < N; i++)
 for (int j = 0; j < N; j++)
-a[i][j] = i*N + j;
+    a[i][j] = i*N + j;
 
 int pos = 0;
+    
 while(flags[i][j] == 0)
 {
     while (j < N && flags[i][j] == 0)
@@ -37,6 +42,7 @@ while(flags[i][j] == 0)
             flags[i][j] = 1;
             j++;
         }
+    
     j--;
     i++;
 
@@ -56,22 +62,22 @@ while(flags[i][j] == 0)
                 flags[i][j] = 1;
                 j--;
              }
-j++;
-i--;
+    j++;
+    i--;
+    
     while (i >= 0 && flags[i][j] == 0)
              {
                 b[pos++] = a[i][j];
                 flags[i][j] = 1;
                 i--;
              }
-i++;
-j++;
+    i++;
+    j++;
 }
 
 for (int i = (N*N) - 1; i >= 0; i--)
-{
     printf("%d%c",b[i],' ');
-}
+
 
            free(a);
            free(b);
