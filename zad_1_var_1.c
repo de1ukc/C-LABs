8ae35f46be2c45685dcf24e784e10cac78499538
Aleh <Trapper.MD@mail.ru>
2021-04-14 21:14:31 +0300

    lab_4_var1_ Asadchy Aleh 053506

---
+++ zad_1_var_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#define LONG_STR 80 /* длина вводимой строки */
+#define MAX_WORD 40 /* максимальное количество слов в строке */
+//ctrl+z == остановить ввод
+void main(void)
+{
+    char *word[MAX_WORD], separator[] = " ,.?!;:", str[LONG_STR];
+    int k = 0;
+    while(1)
+    {
+        char *str = malloc(sizeof(char) * LONG_STR);
+        if(!gets(str)) break;
+
+        str[LONG_STR - 1] = '\0';
+        char *p = strtok(str, separator);
+        while (p) {
+            word[k++] = p;
+            p = strtok(NULL, separator);
+        }
+    }
+    for(int i = 0; i < k; i++) {
+        for(int j = i + 1; j < k; j++) if(strlen(word[i]) > strlen(word[j]) ){
+            char *tmp;
+            tmp = word[j];
+            word[j] = word[i];
+            word[i] = tmp;
+        }
+    }
+   // printf("%d%c", k, '\n');
+    for (int i = 0; i < k; i++) {
+        puts(word[i]);
+//        free(word[i]);
+    }
+
+}
---
+++ zad_2_var_1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 128 /* количество символов в строке */
+int check(char *a, char *b) {
+    if(strlen(a) != strlen(b)) {
+        return 0;
+    }
+    int n = strlen(a);
+    for(int i = 0; i < n - i - 2; i++) if(a[i] != b[n - i - 2]) return 0;
+    return 1;
+}
+void main(void)
+{
+    FILE *fp;
+    fp = fopen("000.txt", "r");
+    if (!fp) exit(1); /* если файл не найден Ц выход */
+    char *word[80];
+    int k =0;
+    while (1){
+
+        char *buffer = malloc(sizeof(char) * N);
+        if(fgets(buffer, N, fp) == NULL) break;
+        word[k++] = buffer;
+    }
+    for(int i = 0; i < k; i++)
+        for(int j = i + 1; j < k; j++) if(check(word[i], word[j]))
+        {
+            puts(word[i]);
+            puts(word[j]);
+            printf("%c", '\n');
+        }
+    fclose(fp);
+}
