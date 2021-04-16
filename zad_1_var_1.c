433afc57ba96d8e062d3e3501f2cdca1421e6200
Aleh <79203780+de1ukc@users.noreply.github.com>
2021-04-16 12:09:57 +0300

    norm memory

--- zad_1_var_1.c
+++ zad_1_var_1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#define LONG_STR 80 
-#define MAX_WORD 40 
-//ctrl+z == end of entering 
+#define LONG_STR 80 /* длина вводимой строки */
+#define MAX_WORD 40 /* максимальное количество слов в строке */
+//ctrl+z == остановить ввод
 void main(void)
 {
     char *word[MAX_WORD], separator[] = " ,.?!;:", str[LONG_STR];
@@ -10,14 +10,16 @@ void main(void)
     while(1)
     {
         char *str = malloc(sizeof(char) * LONG_STR);
-        if(!gets(str)) break;
+        if(!gets(str)) {free(str); break;}
 
         str[LONG_STR - 1] = '\0';
         char *p = strtok(str, separator);
         while (p) {
-            word[k++] = p;
+            word[k++] = (char*)malloc (strlen(p));
+            strcpy(word[k-1],p);
             p = strtok(NULL, separator);
         }
+        free(str);
     }
     for(int i = 0; i < k; i++) {
         for(int j = i + 1; j < k; j++) if(strlen(word[i]) > strlen(word[j]) ){
@@ -28,8 +30,7 @@ void main(void)
         }
     }
    // printf("%d%c", k, '\n');
-    for (int i = 0; i < k; i++) 
-    {
+    for (int i = 0; i < k; i++) {
         puts(word[i]);
         free(word[i]);
     }
