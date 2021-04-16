b082a16259e4a937a85039bf2bee73389e8014bd
Aleh <79203780+de1ukc@users.noreply.github.com>
2021-04-16 14:59:09 +0300

    my own functions

--- zad_2_var_1.c
+++ zad_2_var_1.c
@@ -3,13 +3,39 @@
 #include <string.h>
 #define N 128 /* количество символов в строке */
 int check(char *a, char *b) {
-    if(strlen(a) != strlen(b)) {
+    if(my_strlen(a) != my_strlen(b)) {
         return 0;
     }
-    int n = strlen(a);
+    int n = my_strlen(a);
     for(int i = 0; i < n - i - 2; i++) if(a[i] != b[n - i - 2]) return 0;
     return 1;
 }
+
+int my_strlen(char * c)
+{ int counter = 0 ;
+int i = 0;
+    while (c[i] != '\0')
+    {
+     counter++;
+     i++;
+    }
+return counter;
+}
+
+char * my_strcpy(char *destination, char * source)
+{
+char *ans = destination;
+while (*source != '\0')
+{
+    *destination = *source;
+    destination++;
+    source++;
+}
+*destination = '\0';
+return ans;
+
+}
+
 void main(void)
 {
     FILE *fp;
@@ -22,8 +48,8 @@ void main(void)
         char *buffer = malloc(sizeof(char) * N);
         if(fgets(buffer, N, fp) == NULL) break;
         //word[k++] = buffer;
-        word[k++] = (char*)malloc(strlen(buffer));
-        strcpy(word[k-1],buffer);
+        word[k++] = (char*)malloc(my_strlen(buffer));
+        my_strcpy(word[k-1],buffer);
         free(buffer);
     }
     for(int i = 0; i < k; i++)
