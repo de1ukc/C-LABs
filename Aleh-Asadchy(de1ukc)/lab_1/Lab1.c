8de6f0d3f3b09fa99f157679ec310bdb4fffd27c
Aleh <79203780+de1ukc@users.noreply.github.com>
2021-04-17 21:07:46 +0300

    Aleh Asadchy Lab1 var 19 (#5)
    * Create Lab1.c
    
    Lab1 initial commit
    
    * Update Lab1.c
    
    * Rename Aleh-Asadchy/lab_1/Lab1.c to Aleh-Asadchy(de1ukc)/lab_1/Lab1.c

---
+++ Aleh-Asadchy(de1ukc)/lab_1/Lab1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// var 1.2 (19)
+int main(){
+    int n;
+    while (1){
+        printf("Enter the number: ");
+        scanf("%d", &n);
+        int a = 0, b = 0;
+        if(n < 8) {
+            printf("Incorrect enter!");
+            exit(0);
+    }
+    while(n % 3 != 0){
+        b++;
+        n -= 5;
+    }
+    a += n/ 3;
+/*
+        int k;       //if you need to take as 5 (for exaple 100)
+        k = a / 5;
+        a -= k * 5;
+        b += k * 3;
+*/
+    printf("%s", "Treshek: ");
+    printf("%d", a);
+    printf("%s", "\nPyatereshek: ");
+    printf("%d", b);
+    printf("\n ");
+    }
+}
