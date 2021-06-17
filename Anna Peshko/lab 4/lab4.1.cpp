332895af9e6b88de5c073fed433420cd6d990418
Aleh <Trapper.MD@mail.ru>
2021-06-18 01:48:16 +0300

    Peshko lab 4

---
+++ Anna Peshko/lab 4/lab4.1.cpp
@@ -0,0 +1,47 @@
+﻿//Task 4.1 Variant 2
+
+/*По правилам машинописи после запятой, двоеточия и точки с
+запятой в тексте всегда ставится пробел. После восклицательного,
+вопросительного знака и точки должен стоять пробел, а затем текст
+начинается с заглавной буквы. Составить программу исправления
+такого типа ошибок в тексте.*/
+
+#include <stdio.h>
+
+char NewString(char str[], int a, int b);
+
+void main(void) {
+	int i = 0;
+
+	int pos = 0;
+	char str[256], symb;
+	printf("Enter string:\n");
+	for (i; (symb = getchar()) != '\n'; i++) {
+		str[i] = symb;
+	}
+	str[i] = '\0';
+	for (int j = 0; str[j] != '\0'; j++)
+		pos = j + 1;
+	for (int j = 0; str[j] != '\0'; j++) {
+		if (str[j] == '.' || str[j] == ';' || str[j] == '?' || str[j] == ':' || str[j] == '!' || str[j] == ',') {
+			j++;
+			if (str[j] >= 97 && str[j] <= 122 && (str[j - 1] == '.' || str[j - 1] == '!' || str[j - 1] == '?'))
+			{
+				str[j] = str[j] - 32;
+			}
+			if (str[j] != ' ') {
+				pos++;
+				NewString(str, j, pos);
+			}
+		}
+	}
+	printf("%s", str);
+}
+
+char NewString(char str[], int a, int k) {
+	int f = a;
+	for (k; k > a; k--)
+		str[k] = str[k - 1];
+	str[f] = ' ';
+	return *str;
+}
---
+++ Anna Peshko/lab 4/lab4.2.cpp
@@ -0,0 +1,91 @@
+﻿//Task 4.2 Variant 2
+
+/*Текст в файле содержит многократно вложенные круглые скобки.
+Если баланс скобок соблюден, исправить текст, оставив скобки
+первого уровня круглыми, второго – заменить на квадратные,
+третьего и последующих – на фигурные.*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+
+bool check(char str[])
+{
+	int counter = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '(')
+			counter++;
+		if (str[i] == ')')
+			counter--;
+	}
+	if (counter != 0) {
+		printf("the balance of the brackets is not met");
+		return false;
+	}
+	return true;
+}
+
+void edit(char str[], int& count, int& maxLeinght) {
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '(')
+		{
+			str[i] = '{';
+			count++;
+			if (count > maxLeinght)
+			{
+				maxLeinght = count;
+			}
+		}
+		if (str[i] == ')')
+		{
+			str[i] = '}';
+			count--;
+		}
+	}
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '{')
+		{
+			count++;
+			if (count == maxLeinght) str[i] = '(';
+			if (count == maxLeinght - 1) str[i] = '[';
+		}
+		if (str[i] == '}')
+		{
+			if (count == maxLeinght) str[i] = ')';
+			if (count == maxLeinght - 1) str[i] = ']';
+			count--;
+		}
+	}
+}
+
+
+int main(FILE* fp)
+{
+	char* string = (char*)calloc(255, 1);
+	int count = 0, maxLeinght = 0;
+
+	FILE* my_file;
+	my_file = fopen("input.txt", "r");
+	if (my_file == NULL)
+	{
+		printf("error");
+	}
+	while (fgets(string, 50, my_file) != NULL) {}
+	printf("%s", string);
+	fclose(my_file);
+		
+	if (check(string)) {
+		edit(string, count, maxLeinght);
+		puts(string);
+	}
+
+	free(string);
+}
